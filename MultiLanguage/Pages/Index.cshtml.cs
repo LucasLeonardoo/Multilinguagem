b264de6ba0e03290be6ee36988a9c37967d0c0db
lucas_leo <lucas.leonardo@pacerwm.com>
2020-06-04 15:59:15 -0300

    primeiro commit

---
+++ MultiLanguage/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+using MultiLanguage;
+using System.Diagnostics;
+
+namespace MultiLanguage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IStringLocalizer<SharedResource> sharedResouce;
+        public IndexModel(IStringLocalizer<SharedResource> _sharedResouce)
+        {
+            sharedResouce = _sharedResouce;
+        }
+        public void OnGet()
+        {
+            var teste = sharedResouce["Msg"];
+            Debug.WriteLine("Msg: " + teste);
+        }
+    }
+}
---
+++ MultiLanguage/Startup.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MultiLanguage
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(/*opt => opt.ResourcesPath = "Resources"*/);
+
+            services.AddRazorPages()
+                    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                    .AddDataAnnotationsLocalization();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("pt-BR"),
+                new CultureInfo("en-US")
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
